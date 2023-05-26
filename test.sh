511ed73e17434f780569a8927ac6928cbb39aa31
Artur-Lambo <lamboartur94@gmail.com>
2023-05-26 14:54:54 +0200

    modification du fichier python

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-$(which python3) test.py
+# Définir le chemin d'accès complet au fichier script.py
+script_path="$getPrivateGithubRepo/test.py"
 
+# Exécuter le script en utilisant python3 avec le chemin d'accès complet
+$(which python3) "$script_path"
 
