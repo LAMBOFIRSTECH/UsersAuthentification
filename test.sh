197625da909f0ad0693f945588a539d3544dee59
Artur-Lambo <lamboartur94@gmail.com>
2023-05-26 15:07:28 +0200

    modification du fichier python

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Définir le chemin d'accès complet au fichier script.py
-script_path=pwd
+script_path=./
 
+cd $script_path
 # Exécuter le script en utilisant python3 avec le chemin d'accès complet
-$(which python3) "$script_path/test.py"
+$(which python3) test.py
